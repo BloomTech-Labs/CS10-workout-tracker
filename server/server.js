31ae82513737461086675a013f37cbcb9a6fc7c3
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-10 13:01:15 -0500

    conformed to formatting

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8080;
-require('dotenv').config();
-mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds251622.mlab.com:51622/workout_tracker`, { useNewUrlParser: true }, () => {console.log('connected to mongo')});
-app.listen(port, () => {console.log("server running on port 8080")});
+require("dotenv").config();
+mongoose.connect(
+  `mongodb://${process.env.USERNAME}:${
+    process.env.PASSWORD
+  }@ds251622.mlab.com:51622/workout_tracker`,
+  { useNewUrlParser: true },
+  () => {
+    console.log("connected to mongo");
+  }
+);
+app.listen(port, () => {
+  console.log("server running on port 8080");
+});
