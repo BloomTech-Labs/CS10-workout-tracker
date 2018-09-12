30735ae97643ff0f02344b6043f387b17e0a3956
Xang Thao <xangthao24@yahoo.com>
2018-09-12 12:23:42 -0700

    Created Workout model

---
+++ server/api/models/Workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  exercises: [
+    {
+      exercise: {
+        type: Schema.Types.ObjectId,
+        ref: "exercises"
+      }
+    }
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  }
+});
+
+const WorkoutSchema = mongoose.model("User", WorkoutSchema);
+module.exports = WorkoutSchema;
