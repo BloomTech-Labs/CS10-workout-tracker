9cd4c068d13d2f780011e029f475848c0977be17
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-09-12 13:51:20 -0700

    Added conditional rendering of login/register/logout on the NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,29 +4,38 @@ import { NavLink } from "react-router-dom";
 import { logout } from "../actions";
 
 class NavBar extends Component {
-
-  handleLogout = (event) => {
+  handleLogout = event => {
     this.props.logout();
-  }
+  };
   render() {
     return (
       <div className="NavBar">
         <div className="NavBar__Links">
+          {this.props.authenticated ? null : (
+            <div>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/register">Register</NavLink>
+            </div>
+          )}
           <NavLink to="/schedule">Calendar</NavLink>
           <NavLink to="/workouts">Workouts</NavLink>
           <NavLink to="/progress">Progress</NavLink>
           <NavLink to="/settings">Settings</NavLink>
           <NavLink to="/billing">Billing</NavLink>
         </div>
-        <button className="LogoutButton" onClick={this.handleLogout}>Log Out</button>
+        {this.props.authenticated ? (
+          <button className="LogoutButton" onClick={this.handleLogout}>
+            Log Out
+          </button>
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    state
+    authenticated: state.auth.authed
   };
 };
 
