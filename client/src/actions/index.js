522dd4590ed24b09b9f5b95a88c03e1d1c26bdd5
Amanda Phillips <amanda@phillipsdev.org>
2018-09-12 17:42:49 -0500

    added action changePassword

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -56,3 +56,26 @@ export const logout = () => {
     type: Actions.LOGOUT
   };
 };
+
+export const changePassword = data => {
+  return dispatch => {
+    dispatch({
+      type: Actions.CHANGING_PASSWORD,
+      payload: "Changing password..."
+    });
+    axios
+      .post(`${ROOT_URL}/changepw`, data)
+      .then(res => {
+        dispatch({
+          type: Actions.CHANGE_SUCCESS,
+          payload: res
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: Actions.CHANGE_FAILURE,
+          payload: err
+        });
+      });
+  };
+};
