32c14391eac996bb99504ee469e51da67c23d07b
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-16 13:59:13 -0500

    removed another console.log & conformed to proper formatting

--- client/src/reducers/progress.js
+++ client/src/reducers/progress.js
@@ -1,53 +1,55 @@
 import * as Actions from "../actions/actionDefinitions";
 
 const initialState = {
-    progressRecords: [],
-    message: ""
+  progressRecords: [],
+  message: ""
 };
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case Actions.ADDING_PROGRESS:
-            return {
-                ...state,
-                message: action.payload
-            };
-        case Actions.ADD_PROGRESS_SUCCESS:
-        console.log("ACTION PAYLOAD: " + Array.isArray(action.payload.data.progress))
-            return {
-                ...state,
-                message: "Progress submitted successfully!",
-                progressRecords: [...state.progressRecords, action.payload.data.progress]
-            }
-        case Actions.ADD_PROGRESS_FAILURE:
-            return {
-                ...state,
-                message: "Progress submission failed..."
-            }
-        case Actions.FETCHING_PROGRESS:
-            return {
-                ...state,
-                message: action.payload
-            }
-        case Actions.FETCH_PROGRESS_SUCCESS:
-            return {
-                ...state,
-                progressRecords: action.payload.data.progress,
-                message: "Progress fetched successfully!"
-            }
-        case Actions.FETCH_PROGRESS_FAILURE:
-            return {
-                ...state,
-                message: "Progress fetching failed..."
-            }
-        case Actions.DELETE_PROGRESS:
-            return {
-                ...state,
-                progressRecords: state.progressRecords.filter(record => {
-                    return record._id !== action.payload;
-                })
-            }
-        default: 
-            return state;
-    }
-}
+  switch (action.type) {
+    case Actions.ADDING_PROGRESS:
+      return {
+        ...state,
+        message: action.payload
+      };
+    case Actions.ADD_PROGRESS_SUCCESS:
+      return {
+        ...state,
+        message: "Progress submitted successfully!",
+        progressRecords: [
+          ...state.progressRecords,
+          action.payload.data.progress
+        ]
+      };
+    case Actions.ADD_PROGRESS_FAILURE:
+      return {
+        ...state,
+        message: "Progress submission failed..."
+      };
+    case Actions.FETCHING_PROGRESS:
+      return {
+        ...state,
+        message: action.payload
+      };
+    case Actions.FETCH_PROGRESS_SUCCESS:
+      return {
+        ...state,
+        progressRecords: action.payload.data.progress,
+        message: "Progress fetched successfully!"
+      };
+    case Actions.FETCH_PROGRESS_FAILURE:
+      return {
+        ...state,
+        message: "Progress fetching failed..."
+      };
+    case Actions.DELETE_PROGRESS:
+      return {
+        ...state,
+        progressRecords: state.progressRecords.filter(record => {
+          return record._id !== action.payload;
+        })
+      };
+    default:
+      return state;
+  }
+};
