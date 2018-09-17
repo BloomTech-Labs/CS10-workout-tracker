b9b5989c75627d682d15e55a3080f65f957d525c
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-17 16:17:58 -0500

    added some validation & modified progress reducer action now grabs the nested properties from response object and send to reducer

--- client/src/components/Progress/ProgressTracker.js
+++ client/src/components/Progress/ProgressTracker.js
@@ -7,21 +7,21 @@ class ProgressTracker extends Component {
   render() {
     let date = (this.props.progressRecords[0] || {}).date || "";
     let newDate = new Date(date);
-    let month = newDate.getMonth();
+    let month = newDate.getMonth() + 1;
     let day = newDate.getDate();
     let year = newDate.getFullYear();
     let formattedDate = month + "/" + day + "/" + year;
 
     let weightLost;
     this.props.progressRecords.length <= 1
-      ? (weightLost = "* calculation requires at least 2 progress reports")
+      ? (weightLost = <div className="error">* calculation requires at least 2 progress reports</div>)
       : (weightLost =
          (((this.props.progressRecords[0] || {}).weight || "") -
           ((this.props.progressRecords[this.props.progressRecords.length - 1] || {}).weight || "")) + " lbs");
 
     let inchesLost;
     this.props.progressRecords.length <= 1
-      ? (inchesLost = "* calculation requires at least 2 progress reports")
+      ? (inchesLost = <div className="error">* calculation requires at least 2 progress reports</div>)
       : (inchesLost =
          (((this.props.progressRecords[0] || {}).waist || "") -
           ((this.props.progressRecords[this.props.progressRecords.length - 1] || {}).waist || "")) + " inches");
@@ -30,13 +30,13 @@ class ProgressTracker extends Component {
       <div className="progress-container">
         <div className="progress-box border">
           <div className="number-lost">{weightLost}</div>
-          <div>Weight Lost Since</div>
-          <div>{formattedDate}</div>
+          <div>Weight Lost Since</div> 
+          {isNaN(month) ? null : <div>{formattedDate}</div> }
         </div>
         <div className="progress-box">
           <div className="number-lost">{inchesLost}</div>
           <div>Inches Lost Since</div>
-          <div>{formattedDate}</div>
+          {isNaN(month) ? null : <div>{formattedDate}</div> }
           <span className="disclaimer">* around waist</span>
         </div>
       </div>
