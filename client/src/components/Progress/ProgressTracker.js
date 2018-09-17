fd15abe2b9f02b1947e6bb882b302b6627da910c
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-17 17:38:42 -0500

    changed wording of error

--- client/src/components/Progress/ProgressTracker.js
+++ client/src/components/Progress/ProgressTracker.js
@@ -14,34 +14,72 @@ class ProgressTracker extends Component {
 
     let weightLost;
     this.props.progressRecords.length <= 1
-      ? (weightLost = <div className="error">* calculation requires at least 2 progress reports</div>)
+      ? (weightLost = (
+          <div className="error">
+            * calculation requires at least 2 progress submissions.
+          </div>
+        ))
       : (weightLost =
-         (((this.props.progressRecords[0] || {}).weight || "") -
-          ((this.props.progressRecords[this.props.progressRecords.length - 1] || {}).weight || "")) + " lbs");
+          ((this.props.progressRecords[0] || {}).weight || "") -
+          ((
+            this.props.progressRecords[this.props.progressRecords.length - 1] ||
+            {}
+          ).weight || "") +
+          " lbs");
 
     let inchesLost;
     this.props.progressRecords.length <= 1
-      ? (inchesLost = <div className="error">* calculation requires at least 2 progress reports</div>)
+      ? (inchesLost = (
+          <div className="error">
+            * calculation requires at least 2 progress submissions.
+          </div>
+        ))
       : (inchesLost =
-         (((this.props.progressRecords[0] || {}).waist || "") -
-          ((this.props.progressRecords[this.props.progressRecords.length - 1] || {}).waist || "")) + " inches");
-        
+          ((this.props.progressRecords[0] || {}).waist || "") -
+          ((
+            this.props.progressRecords[this.props.progressRecords.length - 1] ||
+            {}
+          ).waist || "") +
+          " inches");
+
     let weightGained = parseInt(weightLost) * -1;
     let inchesGained = parseInt(inchesLost) * -1;
 
     return (
       <div className="progress-container">
         <div className="progress-box border">
-          {/* <div className="number-lost">{parseInt(weightLost) >= 0 ? weightLost : weightGained }</div> */}
-          {typeof(weightLost) != 'string' ? <div className="error">* calculation requires at least 2 progress reports</div> : <div className="number-lost">{parseInt(weightLost) >= 0 ? weightLost : weightGained }</div> }  
-          {parseInt(weightLost) > 0 ? <div>Weight Lost Since</div> : <div>Weight Gained Since</div> }
-          {isNaN(month) ? null : <div>{formattedDate}</div> }
+          {typeof weightLost != "string" ? (
+            <div className="error">
+              * calculation requires at least 2 progress reports
+            </div>
+          ) : (
+            <div className="number-lost">
+              {parseInt(weightLost) >= 0 ? weightLost : weightGained}
+            </div>
+          )}
+          {parseInt(weightLost) > 0 ? (
+            <div>Weight Lost Since</div>
+          ) : (
+            <div>Weight Gained Since</div>
+          )}
+          {isNaN(month) ? null : <div>{formattedDate}</div>}
         </div>
         <div className="progress-box">
-          {/* <div className="number-lost">{parseInt(inchesLost) >= 0 ? inchesLost : inchesGained }</div> */}
-          {typeof(inchesLost) != 'string' ? <div className="error">* calculation requires at least 2 progress reports</div> : <div className="number-lost">{parseInt(inchesLost) >= 0 ? inchesLost : inchesGained }</div> }  
-          { parseInt(inchesLost) > 0 ? <div>Inches Lost Since</div> : <div>Inches Gained Since</div> }
-          { isNaN(month) ? null : <div>{formattedDate}</div> }
+          {typeof inchesLost != "string" ? (
+            <div className="error">
+              * calculation requires at least 2 progress reports
+            </div>
+          ) : (
+            <div className="number-lost">
+              {parseInt(inchesLost) >= 0 ? inchesLost : inchesGained}
+            </div>
+          )}
+          {parseInt(inchesLost) > 0 ? (
+            <div>Inches Lost Since</div>
+          ) : (
+            <div>Inches Gained Since</div>
+          )}
+          {isNaN(month) ? null : <div>{formattedDate}</div>}
           <span className="disclaimer">* around waist</span>
         </div>
       </div>
