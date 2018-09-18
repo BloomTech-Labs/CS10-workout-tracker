101a55fbdd0fc6a5029f18c21102ad758c8f77f0
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-09-18 16:22:26 -0500

    converted progressCard into reactstrap card

--- client/src/components/Progress/ProgressCard.js
+++ client/src/components/Progress/ProgressCard.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Card, CardBody,
+ CardSubtitle, CardText,} from 'reactstrap';
 import { deleteProgress } from "../../actions";
 import "../../less/progressCard.css";
 
 const ProgressCard = props => {
   return (
-    <div className="progress-record">
+    <div> {/*className="progress-record"*/}
+      <Card>
       <button onClick={() => props.deleteProgress(props.record._id)}>
         delete
       </button>
-      <div>{`Weight: ${props.record.weight} lbs`}</div>
+        <CardBody>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>{`Weight: ${props.record.weight} lbs`}</CardText>
+          <CardText>{`Waist: ${props.record.waist}in`}</CardText>
+          <CardText>{`Hips: ${props.record.hips}in`}</CardText>
+          <CardText>{`(R) Arm: ${props.record.r_arm}in`}</CardText>
+          <CardText>{`(L) Arm: ${props.record.l_arm}in`}</CardText>
+          <CardText>{`(R) Leg: ${props.record.r_leg}in`}</CardText>
+          <CardText>{`(L) Leg: ${props.record.l_leg}in`}</CardText>
+        </CardBody>
+      </Card>
+      {/* <div>{`Weight: ${props.record.weight} lbs`}</div>
       <div>{`Waist: ${props.record.waist}in`}</div>
       <div>{`Hips: ${props.record.hips}in`}</div>
       <div>{`(R) Arm: ${props.record.r_arm}in`}</div>
       <div>{`(L) Arm: ${props.record.l_arm}in`}</div>
       <div>{`(R) Leg: ${props.record.r_leg}in`}</div>
-      <div>{`(L) Leg: ${props.record.l_leg}in`}</div>
+      <div>{`(L) Leg: ${props.record.l_leg}in`}</div> */}
     </div>
   );
 };
