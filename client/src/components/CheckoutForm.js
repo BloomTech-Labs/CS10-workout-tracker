ff0c96283d47035f7bdd86f9cd1211f78d576adf
Amanda Phillips <amanda@phillipsdev.org>
2018-09-17 19:09:33 -0500

    Stripe implemented on client side (not tested)

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import { connect } from "react-redux";
+import { processPayment } from "../actions";
 
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
-    this.submit = this.submit.bind(this);
+    this.state = { complete: false };
   }
 
-  async submit(ev) {
-    // User clicked submit
+  handleSubmit(event) {
+    event.preventDefault();
+    let { stripeToken } = this.props.stripe.createToken({ name: "Name" });
+    let response = processPayment(stripeToken.id);
+    if (response.ok) this.setState({ complete: true });
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
@@ -22,4 +28,18 @@ class CheckoutForm extends Component {
   }
 }
 
-export default injectStripe(CheckoutForm);
+const mapStateToProps = state => {
+  console.log(
+    "At time of render, Registration Page received this app state:",
+    state
+  );
+  return {
+    userInfo: state.auth.currentUser,
+    msg: state.auth.message
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { processPayment }
+)(injectStripe(CheckoutForm));
--- server/api/controllers/UserControllers.js
+++ server/api/controllers/UserControllers.js
@@ -220,6 +220,23 @@ const changeEmail = (req, res) => {
   );
 };
 
+// const billing = (req, res) => {
+//   // Set your secret key: remember to change this to your live secret key in production
+//   // See your keys here: https://dashboard.stripe.com/account/apikeys
+//   var stripe = require("stripe")("sk_test_FijpaH0Y1pepqb8AULRvTHdZ");
+
+//   // Token is created using Checkout or Elements!
+//   // Get the payment token ID submitted by the form:
+//   const token = request.body.stripeToken; // Using Express
+
+//   const charge = stripe.charges.create({
+//     amount: 999,
+//     currency: 'usd',
+//     description: 'Example charge',
+//     source: token,
+//   });
+// }
+
 module.exports = {
   register,
   login,
