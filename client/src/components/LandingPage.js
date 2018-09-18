e182d6554bcae53ec2e3b9eb6f452b18edbe6622
Xang Thao <xangthao24@yahoo.com>
2018-09-18 10:58:47 -0700

    Completed footer design for landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -69,6 +69,9 @@ class LandingPage extends Component {
             </div>
           </div>
         </section>
+        <footer>
+          <p>&copy; Workout Tracker 2018</p>
+        </footer>
       </div>
     );
   }
