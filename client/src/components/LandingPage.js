3796c4d602f213ad638e8ef80646602b39e02571
Xang Thao <xangthao24@yahoo.com>
2018-09-18 21:50:22 -0700

    Added react-animate-on-scroll and animate.css library and completed animations and landing page prototype

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import MainLandingImg from "../img/main_landing.png";
 import "../less/landing.css";
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 import FontAwesome from "react-fontawesome";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -10,7 +12,12 @@ import { faCalendarAlt, faCreditCard, faDumbbell} from '@fortawesome/free-solid-
 library.add(faCalendarAlt, faCreditCard, faDumbbell);
 // library.add(FontAwesomeIcon);
 
+
 class LandingPage extends Component {
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
       <div className="main__landing__page">
@@ -21,29 +28,36 @@ class LandingPage extends Component {
             <span>Login</span>
           </div>
         </nav>
+
         <section className="main__landing__background">
           <div className="container">
             <div className="col-2">
-              <div className="main__content__text">
-                <h1><span className="primary__color__font">WELCOME TO </span>WORKOUT TRACKER</h1>
-                <p>Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum has been the industry's.</p>
-              </div>
+                <div className="main__content__text">
+                <ScrollAnimation animateIn="fadeInUp" >
+                  <h1><span className="primary__color__font">WELCOME TO </span>WORKOUT TRACKER</h1>
+                  <p>Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum has been the industry's.</p>
+                  </ScrollAnimation>
+                </div>
               <img className="main__landing__image" src={MainLandingImg} alt="Main Workout"/>
             </div>
           </div>
         </section>
 
-
         <section className="about__app__landing">
           <div className="container">
+          <ScrollAnimation animateIn="fadeInUp" >
             <div className="display__more">
               <div className="container">
                 <img src={MainLandingImg} alt="Read more"/>
                 <p>Lorem Ipsum has been the industry's standard dummy. this is a test to test.  Lorem Ipsum has been the industry's standard dummy. this is a test to test.  Lorem Ipsum has been the industry's.</p>
               </div>
             </div>
+          </ScrollAnimation>
           </div>
-          <h2>About the app</h2>
+          <ScrollAnimation animateIn="fadeInUp" >
+            <h2>About the app</h2>
+          </ScrollAnimation>
+          <ScrollAnimation animateIn="fadeInUp">
           <div className="landing__icons">
             <div className="container">
               <FontAwesomeIcon icon="calendar-alt" />
@@ -51,24 +65,34 @@ class LandingPage extends Component {
               <FontAwesomeIcon icon="dumbbell" />
             </div>
           </div>
+          </ScrollAnimation>
         </section>
+
         <section>
-          <div className="landing__quote">
-            <h2>
-              <strong>"PAIN IS WEAKNESS LEAVING THE BODY"</strong>
-            </h2>
+          <div 
+            className="landing__quote" >
+              <h2>
+                <strong><ScrollAnimation animateIn="fadeInUp">"PAIN IS WEAKNESS LEAVING THE BODY"</ScrollAnimation></strong>
+              </h2>
+            
           </div>
         </section>
+
         <section className="landing__start">
           <div className="landing__start__content">
+          <ScrollAnimation animateIn="fadeInUp">
             <h2>How To Start</h2>
+          </ScrollAnimation>
             <div>
-              <p><span>1</span> signup</p>
-              <p><span>2</span> Input data</p>
+            <ScrollAnimation animateIn="fadeInUp">
+              <p><span>1</span> Signup</p>
+              <p><span>2</span> Add data</p>
               <p><span>3</span> Workout</p>
+            </ScrollAnimation>
             </div>
           </div>
         </section>
+
         <footer>
           <p>&copy; Workout Tracker 2018</p>
         </footer>
