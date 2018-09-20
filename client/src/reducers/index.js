faef3f2157f36c8461821062502d2fce52bbbce2
LeonOfTheHollow <russell.l.bates@gmail.com>
2018-09-20 10:23:53 -0700

    Added missing import statement to reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import authReducer from "./authentication";
 import progressReducer from "./progress";
 import userReducer from "./user";
+import workoutReducer from "./workouts";
 
 export default combineReducers({
   auth: authReducer,
