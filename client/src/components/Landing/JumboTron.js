f36587aae9be1defab57f302253c40a8f87112ba
LeonOfTheHollow <russell.l.bates@gmail.com>
2018-10-05 12:07:05 -0700

    Added Google fonts

--- client/src/components/Landing/JumboTron.js
+++ client/src/components/Landing/JumboTron.js
@@ -11,15 +11,9 @@ class JumboTron extends React.Component {
         <div className="container">
           <div className="col__2">
               <div className="main__content__text">
-<<<<<<< HEAD
               <ScrollAnimation animateIn="fadeInUp" >
                 <h1><span className="primary__color__font">READY TO BRAWN UP?</span></h1><h1>BRAWNDO.APP</h1>
                 <p>Manage and track your workouts anywhere, anytime, from any device.</p>
-=======
-              <ScrollAnimation animateIn="fadeInUp">
-                <h1><span className="primary__color__font">WELCOME TO </span>WORKOUT TRACKER</h1>
-                <p>Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum has been the industry's.</p>
->>>>>>> 9fe1140976da660e8a0e40ac205dc6bc59bfec74
                 </ScrollAnimation>
               </div>
             <img className="main__landing__image" src={MainLandingImg} alt="Main Workout"/>
