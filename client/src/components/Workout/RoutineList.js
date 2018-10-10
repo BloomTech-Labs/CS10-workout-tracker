3cc2e940106399a1b14da002e174780fcd8f5136
Xang Thao <xangthao24@gmail.com>
2018-10-09 18:50:43 -0700

    tested stagger with no success

--- client/src/components/Workout/RoutineList.js
+++ client/src/components/Workout/RoutineList.js
@@ -2,18 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { fetchRoutines, selectRoutine } from "../../actions";
+import { TimelineLite } from "gsap";
 
 class RoutineList extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.myTween = new TimelineLite({paused: true});
+    this.myElements = [];
+  }
+
   componentDidMount() {
     this.props.fetchRoutines();
+    this.myTween.staggerTo(this.myElements, 0.5, {y: 0, autoAlpha: 1}, 0.1);
   }
 
   render() {
     const { currentRoutines } = this.props;
     console.log(currentRoutines);
-    let routines = currentRoutines.map(routine => {
+    let routines = currentRoutines.map((routine, index) => {
       return (
         <div
+          ref={div => this.myElements[index] = div}
           key={routine._id}
           className="routine"
           onClick={() => this.props.selectRoutine(routine._id)}
